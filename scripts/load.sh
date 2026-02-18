351e01aa52ee448cb8c2971dcdf6d72a68e96b53
Raul <raul.abdurakhmanov@gmail.com>
2026-02-18 01:06:52 +0100

    move scripts

---
+++ scripts/load.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+make clean
+rm -rf iso_root
+rm image.hdd
+rm image.iso
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+bear -- make && \
+"$SCRIPT_DIR/create-iso.sh" && \
+"$SCRIPT_DIR/mbr.sh" && \
+qemu-system-x86_64 -drive format=raw,file=image.hdd -serial stdio
