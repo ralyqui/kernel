b8d76c00a3b1eb94aeb90c17d2e2ba4e2526342e
Raul <raul.abdurakhmanov@gmail.com>
2026-06-09 14:38:43 -0700

    Fix gdt gp fault

--- src/boot/gdt.asm
+++ src/boot/gdt.asm
@@ -1,12 +1,19 @@
 global load_gdt
-[extern gdt_table]
+[extern gdtr_ptr]
 
 load_gdt:
-  lgdt [gdt_table]
-  mov ax, 0x10
-  mov ds, ax
-  mov es, ax
-  mov fs, ax
-  mov gs, ax
-  mov ss, ax
-  ret
+    lgdt [gdtr_ptr]
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    push 0x08
+    lea rax, [rel .after_flush]
+    push rax
+    retfq
+
+.after_flush:
+    ret
--- src/boot/gdt.c
+++ src/boot/gdt.c
@@ -1,14 +1,23 @@
 #include "gdt.h"
 #include "core/fmt.h"
 
+struct gdtr_t {
+    uint16_t limit;
+    uint64_t base;
+} __attribute__((packed));
+
 extern void load_gdt();
 
 volatile uint64_t gdt_table[3];
+volatile struct gdtr_t gdtr_ptr;
 
 void init_gdt() {
     gdt_table[0] = create_desc(0x0, 0x0, 0x0);
     gdt_table[1] = create_desc(0x08, 0x0, GDT_CODE_PL0);
     gdt_table[2] = create_desc(0x10, 0x0, GDT_DATA_PL0);
+
+    gdtr_ptr.limit = sizeof(gdt_table) - 1;
+    gdtr_ptr.base = (uint64_t)gdt_table;
 
     load_gdt();
 
